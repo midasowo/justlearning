a65b2f33e6c6af35f5b0a8294f0adb055baf656c
midsowo <kmiduch@gmail.com>
2022-01-30 21:21:16 +0100

    second commit

---
+++ src/eu/emidas/Main.java
@@ -0,0 +1,14 @@
+package eu.emidas;
+
+public class Main {
+public int cyfra=64;
+
+    public static void main(String[] args) {
+	System.out.println("Cześć Kamil");
+    int bajt = 129;
+        System.out.println(bajt+2);
+        String slowo = "kanapka";
+        String s = slowo.toUpperCase();
+        System.out.println(s    );
+    }
+}
